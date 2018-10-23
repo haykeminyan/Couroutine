110c8ab31313f71f98cdbb9b5989ca00616e2de1
Hayk Eminyan <43892880+haykeminyan@users.noreply.github.com>
2018-10-23 21:55:18 +0300

    Update 4.cpp

--- 4.cpp
+++ 4.cpp
@@ -16,61 +16,59 @@ struct task {
         bool is_finished;
 	char local_data[128];
 };
+
 static int current_task_i = 0;
 static int task_count;
-#define check_resched {/* For an example: just resched after each line. */	\
+
+
+#define check_resched {						\
+	/* For an example: just resched after each line. */	\
 	int old_i = current_task_i;				\
 	current_task_i = (current_task_i + 1) % task_count;	\
 	if (setjmp(tasks[old_i].env) == 0)			\
-		longjmp(tasks[current_task_i].env, 1);}
+		longjmp(tasks[current_task_i].env, 1); }
 
 int merge_two_files(char *resfilelast,char *newfile,char *resfilenew);
-static void
-my_coroutine(task *tasks)
+		
+static void my_coroutine(task *tasks)
 {
-    check_resched;
-    tasks[current_task_i].t=clock();
-    check_resched;
-    sprintf(tasks[current_task_i].tmp_filename, "tmp%d.tmp",current_task_i);
-    check_resched;
-    sprintf(tasks[current_task_i].local_data, "sort -g %s > %s",tasks[current_task_i].local_filename,tasks[current_task_i].tmp_filename);
-    check_resched;
-    system(tasks[current_task_i].local_data);
-	check_resched;
-    sprintf(tasks[current_task_i].local_data, "cat %s > %s",tasks[current_task_i].tmp_filename,tasks[current_task_i].local_filename);
-    check_resched;
-    system(tasks[current_task_i].local_data);
-	check_resched;
-    sprintf(tasks[current_task_i].local_data, "cat %s",tasks[current_task_i].local_filename);
-    check_resched;
-    system(tasks[current_task_i].local_data);
-    check_resched;
-    tasks[current_task_i].is_finished = true;
-    check_resched;
-    tasks[current_task_i].t=clock()-tasks[current_task_i].t;
-    check_resched;
-    printf("#%d coroutine time is %.2lf\n",current_task_i,tasks[current_task_i].t/CLOCKS_PER_SEC);
-    check_resched;
-    while (true) {
-		bool is_all_finished = true;
-		for (int i = 0; i < task_count; ++i) {
-			if (! tasks[i].is_finished) {
-				fprintf(stderr, "Task_id=%d still active, "\
-					"re-scheduling\n", i);
-				is_all_finished = false;
-				break;
-			}
-		}
-		if (is_all_finished) {
-			fprintf(stderr, "No more active tasks to schedule.\n");
-			return;
-		}
-		check_resched;
+    tasks[current_task_i].t=clock();check_resched;
+    sprintf(tasks[current_task_i].tmp_filename, "tmp%d.tmp",current_task_i);check_resched;
+    sprintf(tasks[current_task_i].local_data, "echo | cat %s | xargs -n1 | sort -g | xargs > %s\n",tasks[current_task_i].local_filename,tasks[current_task_i].tmp_filename);check_resched;
+    system(tasks[current_task_i].local_data);check_resched;
+    sprintf(tasks[current_task_i].local_data, "cat %s > %s\n",tasks[current_task_i].tmp_filename,tasks[current_task_i].local_filename);check_resched;
+    system(tasks[current_task_i].local_data);check_resched;
+    tasks[current_task_i].is_finished = true;check_resched;
+    tasks[current_task_i].t=clock()-tasks[current_task_i].t;check_resched;
+    printf("#%d coroutine time is %.2lf\n",current_task_i,tasks[current_task_i].t/CLOCKS_PER_SEC);check_resched;
+    while (true)
+    {
+	bool is_all_finished = true;
+	for (int i = 0; i < task_count; ++i)
+	{
+	    if (! tasks[i].is_finished)
+	    {
+		fprintf(stderr, "Task_id=%d still active, re-scheduling\n", i);
+		is_all_finished = false;
+		break;
+	    }
 	}
+	if (is_all_finished)
+	{
+	    fprintf(stderr, "No more active tasks to schedule.\n");
+	    return;
+	}
+	check_resched;
+    }
 }
 
 int main(int argc, char **argv)
 {
+	if (argc <= 1)
+	{
+	    printf("No args.\n");
+	    return 0;
+	}
         double t;
         int i,flag=1;
         char tmp_buf[256],*char_tmp;
@@ -79,42 +77,20 @@ int main(int argc, char **argv)
         if(!(resfilelast=(char*)malloc(500*sizeof(char))))  
         {
             printf("Not enough memory!\n");
-            return -1009;
+            return -100;
         }
         resfilenew=resfilelast+250;
         strcpy(resfilelast,"res1.tmp");
         strcpy(resfilenew,"res2.tmp");
-        f0=fopen("1.txt","r");
-        if(f0==NULL)
-        {
-            printf("Error\n");
-            return -1;
-        }
-      
-        if(!(fscanf(f0,"%d",&task_count))==1)
-        {
-            printf("Error3\n");
-            return -2;
-        }
+       	task_count = argc - 1;
         cout<<"task_count="<<task_count <<endl;
         struct task tasks[task_count];
         for (i = 0; i < task_count; ++i) {
                 tasks[i].id = i;
                 tasks[i].is_finished = false;
-                if(!(fscanf(f0,"%s",&tasks[i].local_filename)==1))
-                {
-                    printf("Error2\n");
-                    return -1;
-                    
-                }
+                strcpy(tasks[i].local_filename, argv[i + 1]);
                 setjmp(tasks[i].env);
         }
-        if(flag==1)
-        {
-            fclose(f0);
-            flag=0;
-        }
- 
         t=clock();
         my_coroutine(tasks);
         t=clock()-t;
@@ -124,15 +100,15 @@ int main(int argc, char **argv)
         system(tmp_buf);
         for(i=1;i<task_count;i++)
         {
-            system(tmp_buf);
             merge_two_files(resfilelast,tasks[i].local_filename,resfilenew);
             char_tmp=resfilelast;
             resfilelast=resfilenew;
             resfilenew=char_tmp;
         }
+        sprintf(tmp_buf,"cat %s > result.txt",resfilelast);
         system(tmp_buf);
         system("rm -f *.tmp");
-        printf("Finished\n");
+        printf("Finished. Result in \'result.txt\'\n");
         free(resfilelast);
         return 0;
 }
@@ -160,5 +136,145 @@ int merge_two_files(char *resfilelast,char *newfile,char *resfilenew)
         fclose(fr2);
         return -1;
     }
+    double x,y;
+    if(!(fscanf(fr1,"%lf",&x)==1))
+    {
+        if(!feof(fr1))
+        {    
+            printf("Error10\n");
+            fclose(fr1);
+            fclose(fr2);
+            fclose(fw);
+            return -1;
+        }
+        for(;;)
+        {
+            if(!(fscanf(fr2,"%lf",&y)==1))
+            {
+                if(!feof(fr2))
+                {    
+                    printf("Error110\n");
+                    fclose(fr1);
+                    fclose(fr2);
+                    fclose(fw);
+                    return -1;
+                }
+                break;
+            }
+            fprintf(fw,"%lf ",y);
+        }
+        fclose(fr1);
+        fclose(fr2);
+        fclose(fw);
+        return 0;
+    }
+    if(!(fscanf(fr2,"%lf",&y)==1))
+    {
+        if(!feof(fr2))
+        {    
+            printf("Error111\n");
+            fclose(fr1);
+            fclose(fr2);
+            fclose(fw);
+            return -1;
+        }
+        for(;;)
+        {
+            if(!(fscanf(fr1,"%lf",&x)==1))
+            {
+                if(!feof(fr1))
+                {    
+                    printf("Error112\n");
+                    fclose(fr1);
+                    fclose(fr2);
+                    fclose(fw);
+                    return -1;
+                }
+                break;
+            }
+            fprintf(fw,"%lf ",x);
+        }
+        fclose(fr1);
+        fclose(fr2);
+        fclose(fw);
+        return 0;
+    }
+        for(;;)
+    {
+        if(x<y)
+        {
+            fprintf(fw,"%lf\n",x );
+            if(!(fscanf(fr1,"%lf",&x)==1))
+            {
+                if(!feof(fr1))
+                {    
+                    printf("Error10\n");
+                    fclose(fr1);
+                    fclose(fr2);
+                    fclose(fw);
+                    return -1;
+                }
+                for(;;)
+                {
+                    if(!(fscanf(fr2,"%lf",&y)==1))
+                    {
+                        if(!feof(fr2))
+                        {      
+                            printf("Error110\n");
+                            fclose(fr1);
+                            fclose(fr2);
+                            fclose(fw);
+                            return -1;
+                        }
+                        break;
+                    }
+                    fprintf(fw,"%lf ",y);
+                }
+                fclose(fr1);
+                fclose(fr2);
+                fclose(fw);
+                return 0;
+            }
+        }
+        else
+        {    
+            fprintf(fw,"%lf ",y);
+            if(!(fscanf(fr2,"%lf",&y)==1))
+            {
+                if(!feof(fr2))
+                {    
+                    printf("Error111\n");
+                    fclose(fr1);
+                    fclose(fr2);
+                    fclose(fw);
+                    return -1;
+                }
+                for(;;)
+                {
+                    if(!(fscanf(fr1,"%lf",&x)==1))
+                    {
+                        if(!feof(fr1))
+                        {    
+                            printf("Error112\n");
+                            fclose(fr1);
+                            fclose(fr2);
+                            fclose(fw);
+                            return -1;
+                        }
+                        break;
+                    }
+                    fprintf(fw,"%lf ",x);
+                }
+                fclose(fr1);
+                fclose(fr2);
+                fclose(fw);
+                return 0;
+            }
+        }
+    }
+    fclose(fr1);
+    fclose(fr2);
+    fclose(fw);
     return 0;
 }
+        
