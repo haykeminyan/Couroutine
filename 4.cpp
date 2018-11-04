1f8a8141cef44b3f3955e763e2f7700d72181333
Hayk Eminyan <43892880+haykeminyan@users.noreply.github.com>
2018-11-04 16:11:54 +0300

    Update 4.cpp

--- 4.cpp
+++ 4.cpp
@@ -1,230 +1,260 @@
-#include <iostream>
-#include <vector>
-#include <fstream>
-#include <algorithm>
 #include <setjmp.h>
 #include <stdio.h>
 #include <stdbool.h>
-#include <stdio.h>
-#include <iostream>
-#include <chrono>
-
+#include<iostream>
+#include <cstdlib>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
 using namespace std;
 struct task {
-    int id;
-    jmp_buf env;
-    vector<int> v;
-    FILE *fp;
-    int i;
-    int j;
-    long int all_time;
-    int temp;
-    bool is_finished;
-    char local_data[128];
-
+        double t;
+        char local_filename[256];
+        char tmp_filename[256];
+        int id;
+        jmp_buf env;
+        bool is_finished;
+	char local_data[128];
 };
 
-int task_count;
-static struct task *tasks;
 static int current_task_i = 0;
-static std::chrono::high_resolution_clock::time_point start;
-static std::chrono::high_resolution_clock::time_point finish;
-int T;
-
-#define check_resched {                        \
-    finish = std::chrono::high_resolution_clock::now();  \
-    int temp = chrono::duration_cast<std::chrono::nanoseconds>(finish - start).count();\
-    if (temp>=T){  \
-    tasks[current_task_i].all_time += temp; \
-    start = finish;   \
-    int old_i = current_task_i;                \
-    current_task_i = (current_task_i + 1) % task_count;    \
-    if (setjmp(tasks[old_i].env) == 0)            \
-        longjmp(tasks[current_task_i].env, 1); }} \
+static int task_count;
 
 
-static void my_coroutine() {
+#define check_resched {						\
+	/* For an example: just resched after each line. */	\
+	int old_i = current_task_i;				\
+	current_task_i = (current_task_i + 1) % task_count;	\
+	if (setjmp(tasks[old_i].env) == 0)			\
+		longjmp(tasks[current_task_i].env, 1); }
 
-
-    tasks[current_task_i].fp = fopen(tasks[current_task_i].local_data, "r");
-    check_resched;
-    while (fscanf(tasks[current_task_i].fp, "%d", &tasks[current_task_i].temp) != EOF) 
+int merge_two_files(char *resfilelast,char *newfile,char *resfilenew);
+		
+static void my_coroutine(task *tasks)
+{
+    tasks[current_task_i].t=clock();check_resched;
+    sprintf(tasks[current_task_i].tmp_filename, "tmp%d.tmp",current_task_i);check_resched;
+    sprintf(tasks[current_task_i].local_data, "echo | cat %s | xargs -n1 | sort -g | xargs > %s\n",tasks[current_task_i].local_filename,tasks[current_task_i].tmp_filename);check_resched;
+    system(tasks[current_task_i].local_data);check_resched;
+    sprintf(tasks[current_task_i].local_data, "cat %s > %s\n",tasks[current_task_i].tmp_filename,tasks[current_task_i].local_filename);check_resched;
+    system(tasks[current_task_i].local_data);check_resched;
+    tasks[current_task_i].is_finished = true;check_resched;
+    tasks[current_task_i].t=clock()-tasks[current_task_i].t;check_resched;
+    printf("#%d coroutine time is %.2lf\n",current_task_i,tasks[current_task_i].t/CLOCKS_PER_SEC);check_resched;
+    while (true)
     {
-        check_resched;
-        tasks[current_task_i].v.push_back(tasks[current_task_i].temp);
-        check_resched;
+	bool is_all_finished = true;
+	for (int i = 0; i < task_count; ++i)
+	{
+	    if (! tasks[i].is_finished)
+	    {
+		fprintf(stderr, "Task_id=%d still active, re-scheduling\n", i);
+		is_all_finished = false;
+		break;
+	    }
+	}
+	if (is_all_finished)
+	{
+	    fprintf(stderr, "No more active tasks to schedule.\n");
+	    return;
+	}
+	check_resched;
     }
-    check_resched;
+}
 
-    //Try to realize Bubble sort
-    for (tasks[current_task_i].i = 0;
-         tasks[current_task_i].i < tasks[current_task_i].v.size() - 1; tasks[current_task_i].i++) {
-        for (tasks[current_task_i].j = 0; tasks[current_task_i].j < tasks[current_task_i].v.size() - 1 -
-                                                                    tasks[current_task_i].i; tasks[current_task_i].j++) 
+int main(int argc, char **argv)
+{
+	if (argc <= 1)
+	{
+	    printf("No args.\n");
+	    return 0;
+	}
+        double t;
+        int i,flag=1;
+        char tmp_buf[256],*char_tmp;
+        char *resfilelast,*resfilenew;
+        FILE *f0;
+        if(!(resfilelast=(char*)malloc(500*sizeof(char))))  
         {
-            check_resched;
-            if (tasks[current_task_i].v[tasks[current_task_i].j] >
-                tasks[current_task_i].v[tasks[current_task_i].j + 1]) {
-                check_resched;
-                tasks[current_task_i].temp = tasks[current_task_i].v[tasks[current_task_i].j];
-                check_resched;
-                tasks[current_task_i].v[tasks[current_task_i].j] = tasks[current_task_i].v[tasks[current_task_i].j + 1];
-                check_resched;
-                tasks[current_task_i].v[tasks[current_task_i].j + 1] = tasks[current_task_i].temp;
-                check_resched;
-            }
+            printf("Not enough memory!\n");
+            return -100;
         }
-    }
+        resfilenew=resfilelast+250;
+        strcpy(resfilelast,"res1.tmp");
+        strcpy(resfilenew,"res2.tmp");
+       	task_count = argc - 1;
+        cout<<"task_count="<<task_count <<endl;
+        struct task tasks[task_count];
+        for (i = 0; i < task_count; ++i) {
+                tasks[i].id = i;
+                tasks[i].is_finished = false;
+                strcpy(tasks[i].local_filename, argv[i + 1]);
+                setjmp(tasks[i].env);
+        }
+        t=clock();
+        my_coroutine(tasks);
+        t=clock()-t;
+        printf("All time is %.2lf \n",t/CLOCKS_PER_SEC);
+        system("rm -f *.tmp");
+        sprintf(tmp_buf,"cat %s > %s",tasks[0].local_filename,resfilelast);
+        system(tmp_buf);
+        for(i=1;i<task_count;i++)
+        {
+            merge_two_files(resfilelast,tasks[i].local_filename,resfilenew);
+            char_tmp=resfilelast;
+            resfilelast=resfilenew;
+            resfilenew=char_tmp;
+        }
+        sprintf(tmp_buf,"cat %s > result.txt",resfilelast);
+        system(tmp_buf);
+        system("rm -f *.tmp");
+        printf("Finished. Result in \'result.txt\'\n");
+        free(resfilelast);
+        return 0;
+}
 
-    check_resched;
-    fclose(tasks[current_task_i].fp);
-    check_resched;
-    char str[12];
-    sprintf(str, "%d", current_task_i);
-    tasks[current_task_i].fp = fopen(str, "w"); // write mode;
-    check_resched;
-    for (tasks[current_task_i].i = 0;
-         tasks[current_task_i].i < tasks[current_task_i].v.size(); tasks[current_task_i].i++) {
-        check_resched;
-        fprintf(tasks[current_task_i].fp, "%d ", tasks[current_task_i].v[tasks[current_task_i].i]);
-        check_resched;
+int merge_two_files(char *resfilelast,char *newfile,char *resfilenew)
+{     
+    FILE *fr1=fopen(resfilelast,"r");
+    if(fr1==NULL)
+    {
+        printf("Error5\n");
+        return -1;
     }
-    fclose(tasks[current_task_i].fp);
-    check_resched;
-    cout << "Task " << current_task_i << " finished" << endl;
-    cout << "Total time is   " << tasks[current_task_i].all_time << " ns" << endl;
-    tasks[current_task_i].is_finished = true;
+    FILE *fr2=fopen(newfile,"r");
+    if(fr2==NULL)
+    {
+        printf("Error6\n");
+        fclose(fr1);
+        return -1;
+    }
+    FILE *fw=fopen(resfilenew,"w");
+    if(fw==NULL)
+    {
+        printf("Error7\n");
+        fclose(fr1);
+        fclose(fr2);
+        return -1;
+    }
+    double x,y;
+    if(!(fscanf(fr1,"%lf",&x)==1))
+    {
+        if(!feof(fr1))
+        {    
+            printf("Error10\n");
 
-    while (true) {
-        bool is_all_finished = true;
-        for (int i = 0; i < task_count; ++i) {
-            if (!tasks[i].is_finished) {
-                is_all_finished = false;
+            return -1;
+        }
+        for(;;)
+        {
+            if(!(fscanf(fr2,"%lf",&y)==1))
+            {
+                if(!feof(fr2))
+                {    
+                    printf("Error110\n");
+     
+                    return -1;
+                }
                 break;
             }
+            fprintf(fw,"%lf ",y);
         }
-        if (is_all_finished) {
-            return;
+        fclose(fr1);
+        fclose(fr2);
+        fclose(fw);
+        return 0;
+    }
+    if(!(fscanf(fr2,"%lf",&y)==1))
+    {
+        if(!feof(fr2))
+        {    
+            printf("Error111\n");
+
+            return -1;
         }
-        check_resched;
-    }
-}
-
-struct MinHeapNode {
-    int element;
-    int i;
-};
-
-void swap_heap(MinHeapNode *x, MinHeapNode *y) {
-    MinHeapNode temp = *x;
-    *x = *y;
-    *y = temp;
-}
-
-class MinHeap {
-    MinHeapNode *harr;
-    int heap_size;
-
-public:
-    MinHeap(MinHeapNode a[], int size);
-    void MinHeapify(int);
-    int left(int i) { return (2 * i + 1); }
-    int right(int i) { return (2 * i + 2); }
-    MinHeapNode getMin() { return harr[0]; }
-    void replaceMin(MinHeapNode x) {
-        harr[0] = x;
-        MinHeapify(0);
-    }
-};
-
-MinHeap::MinHeap(MinHeapNode a[], int size) {
-    heap_size = size;
-    harr = a;
-    int i = (heap_size - 1) / 2;
-    while (i >= 0) {
-        MinHeapify(i);
-        i--;
-    }
-}
-
-//Recursive function to calculate mininum element of heap
-void MinHeap::MinHeapify(int i) {
-    int l = left(i);
-    int r = right(i);
-    int smallest = i;
-    if (l < heap_size && harr[l].element < harr[i].element)
-        smallest = l;
-    if (r < heap_size && harr[r].element < harr[smallest].element)
-        smallest = r;
-    if (smallest != i) {
-        swap_heap(&harr[i], &harr[smallest]);
-        MinHeapify(smallest);
-    }
-}
-
-void mergeFiles(const string &output_file, int k) {
-    int n = 0;
-    FILE *f = fopen("0", "r");
-    int temp = 0;
-    while (fscanf(f, "%d", &temp) != EOF) {
-        n++;
-    }
-    fclose(f);
-    FILE *in[k];
-    for (int i = 0; i < k; i++) {
-        char fileName[2];
-        snprintf(fileName, sizeof(fileName), "%d", i);
-        in[i] = fopen(fileName, "r");
-    }
-
-    FILE *out = fopen(output_file.c_str(), "w");
-
-    MinHeapNode *harr = new MinHeapNode[k];
-    int i;
-    for (i = 0; i < k; i++) {
-        if (fscanf(in[i], "%d ", &harr[i].element) != 1)
-            break;
-
-        harr[i].i = i;
-    }
-    MinHeap hp(harr, i);
-
-    int count = 0;
-    while (count != i) {
-        MinHeapNode root = hp.getMin();
-        fprintf(out, "%d ", root.element);
-
-        if (fscanf(in[root.i], "%d ", &root.element) != 1) {
-            root.element = INT8_MAX;
-            count++;
+        for(;;)
+        {
+            if(!(fscanf(fr1,"%lf",&x)==1))
+            {
+                if(!feof(fr1))
+                {    
+                    printf("Error112\n");
+                    return -1;
+                }
+                break;
+            }
+            fprintf(fw,"%lf ",x);
         }
-
-        hp.replaceMin(root);
+        fclose(fr1);
+        fclose(fr2);
+        fclose(fw);
+        return 0;
     }
+        for(;;)
+    {
+        if(x<y)
+        {
+            fprintf(fw,"%lf\n",x );
+            if(!(fscanf(fr1,"%lf",&x)==1))
+            {
+                if(!feof(fr1))
+                {    
+                    printf("Error10\n");
+                  
+                    return -1;
+                }
+                for(;;)
+                {
+                    if(!(fscanf(fr2,"%lf",&y)==1))
+                    {
+                        if(!feof(fr2))
+                        {      
+                            printf("Error110\n");
 
-    // close input and output files
-    for (int i = 0; i < k; i++)
-        fclose(in[i]);
-
-    fclose(out);
-}
-
-int main(int argc, char *argv[]) {
-    T = atoi(argv[1]);
-    task_count = argc - 2;
-    tasks = new task[task_count];
-    int i;
-    for (i = 0; i < task_count; ++i) {
-        tasks[i].id = i;
-        tasks[i].is_finished = false;
-        sprintf(tasks[i].local_data, "%s", argv[i + 2]);
-        /* Entry point for new co-coutines. */
-        setjmp(tasks[i].env);
+                            return -1;
+                        }
+                        break;
+                    }
+                    fprintf(fw,"%lf ",y);
+                }
+                fclose(fr1);
+                fclose(fr2);
+                fclose(fw);
+                return 0;
+            }
+        }
+        else
+        {    
+            fprintf(fw,"%lf ",y);
+            if(!(fscanf(fr2,"%lf",&y)==1))
+            {
+                if(!feof(fr2))
+                {    
+                    printf("Error111\n");
+                    return -1;
+                }
+                for(;;)
+                {
+                    if(!(fscanf(fr1,"%lf",&x)==1))
+                    {
+                        if(!feof(fr1))
+                        {    
+                            printf("Error112\n");
+                            return -1;
+                        }
+                        break;
+                    }
+                    fprintf(fw,"%lf ",x);
+                }
+                fclose(fr1);
+                fclose(fr2);
+                fclose(fw);
+                return 0;
+            }
+        }
     }
-    start = std::chrono::high_resolution_clock::now();
-    my_coroutine();
-    mergeFiles("result.dat", task_count);
-    printf("Finished\n");
-
+    fclose(fr1);
+    fclose(fr2);
+    fclose(fw);
     return 0;
 }
